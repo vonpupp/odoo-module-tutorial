b1355409559a4f00d345b925361fb0b4d3da697c
vonpupp <vonpupp@gmail.com>
2016-12-12 23:12:16 -0200

    Automate bootstrap

--- bootstrap-ubuntu-xenial.sh
+++ bootstrap-ubuntu-xenial.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Principais
-sudo apt-get install git python2.7 postgresql nano python-virtualenv xfonts-75dpi
+sudo apt-get install -y git python2.7 postgresql nano python-virtualenv xfonts-75dpi
 # Build
-sudo apt-get install gcc python2.7-dev libxml2-dev \
+sudo apt-get install -y gcc python2.7-dev libxml2-dev \
     libxslt1-dev libevent-dev libsasl2-dev libldap2-dev libpq-dev \
     libpng12-dev libjpeg-dev
 # Ubuntu xenial
-sudo apt install nodejs-legacy
-sudo apt-get install fontconfig xfonts-base
-sudo apt-get install wkhtmltopdf
+sudo apt-get install -y nodejs-legacy
+sudo apt-get install -y fontconfig xfonts-base
+sudo apt-get install -y wkhtmltopdf
 # Node
 sudo apt-get install -y npm
 sudo npm install -g less
