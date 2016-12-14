f776dcbde2eae0ca0de1607b96375fdc5ced076a
vonpupp <vonpupp@gmail.com>
2016-12-14 18:09:30 -0200

    Initial commit from book Odoo 10 Development Essentials

---
+++ specific-parts/specific-addons/todo_app/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 vonpupp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Todo App',
+    'description': """
+        To-do APP""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'vonpupp',
+    'website': 'https://github.com/vonpupp',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
