e1f28af4432097b9719c6da349ebb32e7a8e80a2
vonpupp <vonpupp@gmail.com>
2016-12-16 20:52:58 -0200

    Fail: testing security with regular user

--- specific-parts/specific-addons/todo_app/tests/test_todo.py
+++ specific-parts/specific-addons/todo_app/tests/test_todo.py
@@ -6,6 +6,12 @@ from odoo.exceptions import AccessError
 
 class TestTodo(TransactionCase):
 
+    def setUp(self, *args, **kwargs):
+        result = super(TestTodo, self).setUp(*args, **kwargs)
+        user_demo = self.env.ref('base.user_demo')
+        self.env = self.env(user=user_demo)
+        return result
+
     def test_create(self):
         "Create a simple Todo"
         Todo = self.env['todo.task']
@@ -19,3 +25,10 @@ class TestTodo(TransactionCase):
         # Test clear done
         Todo.do_clear_done()
         self.assertFalse(task.active)
+
+    def test_record_rule(self):
+        "Test per user record rules"
+        Todo = self.env['todo.task']
+        task = Todo.sudo().create({'name': 'Admin Task'})
+        with self.assertRaises(AccessError):
+            Todo.browse([task.id]).name
