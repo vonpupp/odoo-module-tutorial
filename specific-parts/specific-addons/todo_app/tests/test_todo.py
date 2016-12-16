805b4986e140bcf7f0622a0b04ad001d3c3bb277
vonpupp <vonpupp@gmail.com>
2016-12-16 20:08:06 -0200

    Add tests to actions

--- specific-parts/specific-addons/todo_app/tests/test_todo.py
+++ specific-parts/specific-addons/todo_app/tests/test_todo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo.tests.common import TransactionCase
+from odoo.exceptions import AccessError
 
 
 class TestTodo(TransactionCase):
@@ -10,3 +11,11 @@ class TestTodo(TransactionCase):
         Todo = self.env['todo.task']
         task = Todo.create({'name': 'Test Task'})
         self.assertEqual(task.is_done, False)
+
+        # Test toggle done
+        task.do_toggle_done()
+        self.assertTrue(task.is_done)
+
+        # Test clear done
+        Todo.do_clear_done()
+        self.assertFalse(task.active)
