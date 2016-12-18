35b5458802ad3de05f20ca64aa9d9f2777575f81
vonpupp <vonpupp@gmail.com>
2016-12-18 12:27:42 -0200

    Override methods

--- specific-parts/specific-addons/todo_user/models/todo_task.py
+++ specific-parts/specific-addons/todo_user/models/todo_task.py
@@ -1,9 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class TodoTask(models.Model):
     _inherit = 'todo.task'
     name = fields.Char(help="What needs to be done?")
     user_id = fields.Many2one('res.users', 'Responsible')
     date_deadline = fields.Date('Deadline')
+
+    @api.multi
+    def do_clear_done(self):
+        domain = [('is_done', '=', True),
+                   '|', ('user_id', '=', self.env.uid),
+                        ('user_id', '=', False)]
+        dones = self.search(domain)
+        dones.write({'active': False})
+        return True
+
+    @api.multi
+    def do_toggle_done(self):
+        for task in self:
+            if task.user != self.env.user:
+                raise ValidationError(
+                    'Only the responsible can do this!')
+        return super(TodoTask, self).do_toggle_done()
