eb0e2fceeb9bfad128df97581227bdcf12b3db5e
vonpupp <vonpupp@gmail.com>
2016-12-18 22:19:07 -0200

    Add inherited views of the new app

--- specific-parts/specific-addons/todo_user/__manifest__.py
+++ specific-parts/specific-addons/todo_user/__manifest__.py
@@ -1,4 +1,9 @@
-{  'name': 'Multiuser To-Do',
-   'description': 'Extend the To-Do app to multiuser.',
-   'author': 'vonpupp',
-   'depends': ['todo_app'], }
+{
+    'name': 'Multiuser To-Do',
+    'description': 'Extend the To-Do app to multiuser.',
+    'author': 'vonpupp',
+    'depends': ['todo_app'],
+    'data': [
+        'views/todo_task.xml',
+    ]
+}
