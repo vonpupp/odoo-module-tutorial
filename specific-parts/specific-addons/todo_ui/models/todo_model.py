55e66d3b88aa0b3a9c7c28c33a6f5f9e116d9878
vonpupp <vonpupp@gmail.com>
2016-12-25 16:47:28 -0200

    Add fields

--- specific-parts/specific-addons/todo_ui/models/todo_model.py
+++ specific-parts/specific-addons/todo_ui/models/todo_model.py
@@ -1,14 +1,32 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 class Tag(models.Model):
     _name = 'todo.task.tag'
     _description = 'To-do Tag'
     name = fields.Char('Name', 40, translate=True)
 
+
 class Stage(models.Model):
     _name = 'todo.task.stage'
     _description = 'To-do Stage'
     _order = 'sequence,name'
+    # String fields
     name = fields.Char('Name', 40, translate=True)
+    desc = fields.Text('Description')
+    state = fields.Selection([
+        ('draft', 'New'),
+        ('open', 'Started'),
+        ('done', 'Closed')
+        ], 'State')
+    docs = fields.Html('Documentation')
+    # Numeric fields
     sequence = fields.Integer('Sequence')
+    perc_complete = fields.Float('% Complete', (3, 2))
+    # Date fields:
+    date_effective = fields.Date('Effective Date')
+    date_changed = fields.Datetime('Last Changed')
+    # Other fields:
+    fold = fields.Boolean('Folded?')
+    image = fields.Binary('Image')
