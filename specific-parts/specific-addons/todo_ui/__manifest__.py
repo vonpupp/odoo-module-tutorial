571ab5991e4225db9ace703712df532eff1bddbc
vonpupp <vonpupp@gmail.com>
2016-12-25 12:29:53 -0200

    Add base todo_ui module

---
+++ specific-parts/specific-addons/todo_ui/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'User interface improvements to the To-Do app',
+    'description': 'User friendly features.',
+    'author': 'vonpupp',
+    'depends': [
+        'todo_user'
+    ]
+}
