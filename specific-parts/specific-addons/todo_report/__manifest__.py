7ee9b52fe05bbaed86dc9132db1eb3262bdcb8d0
vonpupp <vonpupp@gmail.com>
2017-01-06 11:44:04 -0200

    Add todo_report manifest

---
+++ specific-parts/specific-addons/todo_report/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 vonpupp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Todo Report',
+    'description': """
+        Report for to-do tasks""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'vonpupp',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
